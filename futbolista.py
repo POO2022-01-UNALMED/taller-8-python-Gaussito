00a0585ba383fe2c464e35551495213a8fa42025
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-05-20 13:47:02 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -15,7 +15,7 @@ class Futbolista(Persona, Deportista):
         Futbolista.listaFutbolistas.append(self)
 
     def __str__(self):
-        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAnosPracticando()} años en el deporte"
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
 
     def getGolesMarcados(self):
         return self._golesMarcados
